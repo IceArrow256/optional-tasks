1e5f6de99353642aa4c1f83a325e97f737076922
IceArrow256 <icearrow256@gmail.com>
2021-09-26 15:19:21 +0300

    improve code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use directories::ProjectDirs;
 use serde::{Deserialize, Serialize};
+use std::fmt;
 use std::fs::File;
 use std::io::Read;
+use std::path::PathBuf;
 
 #[derive(Serialize, Deserialize)]
 struct Task {
@@ -9,24 +11,67 @@ struct Task {
     description: String,
 }
 
+#[derive(Serialize, Deserialize)]
+struct Tasks {
+    data: Vec<Task>,
+}
+
+impl Tasks {
+    fn new() -> Tasks {
+        let data: Vec<Task> = Vec::new();
+        Tasks { data }
+    }
+
+    #[allow(dead_code)]
+    fn add(&mut self, id: u16, description: String) {
+        self.data.push(Task { id, description });
+    }
+}
+
+impl fmt::Display for Task {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} {}", self.id, self.description)
+    }
+}
+
+impl fmt::Display for Tasks {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut output = String::new();
+        for task in &self.data {
+            output.push_str(&format!("{}\n", task));
+        }
+        write!(f, "{}", output)
+    }
+}
+
 fn main() {
+    #[allow(unused_mut)]
+    let mut tasks = load_tasks();
+    println!("{}", tasks);
+    save_task(tasks);
+}
+
+fn get_tasks_file_path() -> PathBuf {
     let proj_dirs = ProjectDirs::from("com", "IceArrow256", "optional-tasks").unwrap();
     let data_dir = proj_dirs.data_dir();
-    std::fs::create_dir(data_dir).ok();
-    let mut tasks: Vec<Task> = Vec::new();
-    let paths = std::fs::read_dir(data_dir).unwrap();
-    for path in paths {
+    data_dir.join("tasks.json")
+}
+
+fn load_tasks() -> Tasks {
+    let tasks_file_path = get_tasks_file_path();
+    if tasks_file_path.exists() {
         let mut data = String::new();
-        let mut f = File::open(path.unwrap().path()).expect("Unable to open file");
-        f.read_to_string(&mut data).expect("Unable to read string");
-        tasks.push(serde_json::from_str(&data).ok().unwrap())
+        let mut file = File::open(tasks_file_path).unwrap();
+        file.read_to_string(&mut data)
+            .expect("Unable to read string");
+        serde_json::from_str(&data).ok().unwrap()
+    } else {
+        Tasks::new()
     }
-    for task in tasks {
-        println!("id: {}, description: {}", task.id, task.description);
-        std::fs::write(
-            data_dir.join(task.id.to_string() + ".json"),
-            serde_json::to_string(&task).ok().unwrap(),
-        )
-        .ok();
-    }
+}
+
+fn save_task(tasks: Tasks) {
+    let tasks_file_path = get_tasks_file_path();
+    std::fs::create_dir(tasks_file_path.parent().unwrap()).ok();
+    std::fs::write(tasks_file_path, serde_json::to_string(&tasks).ok().unwrap()).ok();
 }
